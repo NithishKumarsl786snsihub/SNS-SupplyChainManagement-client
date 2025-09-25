2862c15ec7e6fa1731c85234c3a80e672c40f794
nithishkumarr27 <nithish.r.ihub@snsgroups.com>
2025-09-25 15:10:44 +0530

    ADDED THE TYPES

--- src/app/results/arimax/page.tsx
+++ src/app/results/arimax/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react'
-import { UploadResponse, ModelInfo, PredictionResponse, PriceOptimizationResponse, ApiError, DatasetInfo } from '../../types/api'
+import { UploadResponse, ModelInfo, PredictionResponse, PriceOptimizationResponse, ApiError, DatasetInfo } from '../../../types/api'
 import axios from 'axios'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, BarChart, Bar } from 'recharts'
 
